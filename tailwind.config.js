879528cce7e377b38e411263f265487e43ce65c7
Ramses Ramos <148809372+Ramses1406@users.noreply.github.com>
2024-03-18 16:30:53 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      colors: {
+        'primary': {
+          StrongCyan:'hsl(171, 66%, 44%)',
+          LightBlue: 'hsl(233, 100%, 69%)',
+          VeryDarkBlackishBlue:'hsl(219, 29%, 14%)',
+        },
+         'neutral': {
+          DarkGrayishBlue: 'hsl(210, 10%, 33%)',
+          GrayishBlue: 'hsl(201, 11%, 66%)',
+        },
+
+      }
+    },
+  },
+  plugins: [],
+}
+
